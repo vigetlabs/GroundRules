26785d5c9f1648aba57e8d3e6b26a3a55c6ea2c1
Jason Roodman <jason@Jasons-MacBook-Pro.local>
2017-07-11 11:51:02 -0400

    added colonel kurtz blocks

--- app/assets/javascripts/admin/colonel/blockTypes/index.js
+++ app/assets/javascripts/admin/colonel/blockTypes/index.js
@@ -6,6 +6,16 @@ module.exports = [
   {
     id:        'youtube',
     label:     'Youtube',
-    component: require('colonel-kurtz/addons/youtube'),
+    component: require('colonel-kurtz/addons/youtube')
   },
+  {
+    id: 'image',
+    label: 'Image',
+    component: require('colonel-kurtz/addons/image')
+  },
+  {
+    id: 'text',
+    label: 'Text',
+    component : require('colonel-kurtz/addons/medium')
+  }
 ]
