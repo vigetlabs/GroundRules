89c9984070bc75bdaa26514f45400fec310d5898
jroodman <jroodman@users.noreply.github.com>
2017-07-19 17:46:34 -0400

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,19 @@ if (Rails.env != 'production')
     AdminUser.create(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
   end
 
+  device_image = Photo.create(name: 'Device Image') do |photo|
+    photo.image = File.open(Rails.root.join("assets/images/device.jpg"))
+  end
+  roast_image = Photo.create(name: 'Roast Image') do |photo|
+    photo.image = File.open(Rails.root.join("assets/images/roast.jpg"))
+  end
+
   french_press = Device.find_or_create_by(name: 'French Press')
   chemex = Device.find_or_create_by(name: 'Chemex')
   keurig = Device.find_or_create_by(name: 'Keurig')
+  french_press.image = device_image
+  chemex.image = device_image
+  keurig.image = device_image
 
   create_block = -> (problem) { problem.content = { 'Problem' => 'Answer' } }
   DeviceProblem.find_or_create_by(statement: "I'm tired", device: keurig, &create_block)
@@ -17,20 +27,23 @@ if (Rails.env != 'production')
   DeviceProblem.find_or_create_by(statement: "I'm tired", device: french_press, &create_block)
   DeviceProblem.find_or_create_by(statement: "My pants ripped yesterday", device: french_press, &create_block)
   DeviceProblem.find_or_create_by(statement: "I don't have anything better than a keurig", device: french_press, &create_block)
-  
-  Roast.find_or_create_by(
+
+  light_roast = Roast.find_or_create_by(
     name: 'Light Roast',
     toggle_text: 'This is a light roast, really great',
-    description: 'Still a light roast, still really great'
+    description: 'Still a light roast, still really great',
   )
-  Roast.find_or_create_by(
+  medium_roast = Roast.find_or_create_by(
     name: 'Medium Roast',
     toggle_text: 'This is a medium roast, really great',
-    description: 'Still a medium roast, still really great'
+    description: 'Still a medium roast, still really great',
   )
-  Roast.find_or_create_by(
+  dark_roast = Roast.find_or_create_by(
     name: 'Dark Roast',
     toggle_text: 'This is a dark roast, really great',
-    description: 'Still a dark roast, still really great'
+    description: 'Still a dark roast, still really great',
   )
+  light_roast.toggle_image = roast_image
+  medium_roast.toggle_image = roast_image
+  dark_roast.toggle_image = roast_image
 end
