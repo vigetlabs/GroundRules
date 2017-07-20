31f3c00728c82852f84ee479b597276ec04dcbf8
jroodman <jroodman@users.noreply.github.com>
2017-07-20 17:56:57 -0400

    custom 404 and 500

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get '/roasts', to: 'roasts#index'
   get '/devices', to: 'devices#index'
+  match "/404" => "errors#error404", via: :all
+  match "/500" => "errors#error500", via: :all
 
 end
