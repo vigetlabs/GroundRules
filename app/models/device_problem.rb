d13cbed418c5fce7a0e45aec5f9da021c7ff4b97
jroodman <jroodman@users.noreply.github.com>
2017-07-21 10:22:07 -0400

    response to code review

--- app/models/device_problem.rb
+++ app/models/device_problem.rb
@@ -3,14 +3,7 @@ class DeviceProblem < ApplicationRecord
   belongs_to :device
 
   validates :statement, :content, :device_id, presence: true
-
-  validate :statement_within_allowed_length, if: :statement
-
-  def statement_within_allowed_length
-    if statement.length > 35
-      errors.add(:statement, "must contain less than 35 characters")
-    end
-  end
+  validates :statement, length: { maximum: 35 }
 
   def to_s
     statement
