26e95bfdbb9dd1a7154bd9ab384be80d9a444972
jroodman <jroodman@users.noreply.github.com>
2017-07-27 17:26:06 -0400

    remove not_null constraint from position column

--- db/migrate/20170726203902_add_acts_as_list.rb
+++ db/migrate/20170726203902_add_acts_as_list.rb
@@ -1,21 +1,21 @@
 class AddActsAsList < ActiveRecord::Migration[5.1]
   def change
-    add_column :devices, :position, :integer, null: false
+    add_column :devices, :position, :integer
     Device.order(:updated_at).each.with_index(1) do |item, index|
       item.update_column :position, index
     end
 
-    add_column :device_problems, :position, :integer, null: false
+    add_column :device_problems, :position, :integer
     DeviceProblem.order(:updated_at).each.with_index(1) do |item, index|
       item.update_column :position, index
     end
 
-    add_column :roasts, :position, :integer, null: false
+    add_column :roasts, :position, :integer
     Roast.order(:updated_at).each.with_index(1) do |item, index|
       item.update_column :position, index
     end
 
-    add_column :roast_brands, :position, :integer, null: false
+    add_column :roast_brands, :position, :integer
     RoastBrand.order(:updated_at).each.with_index(1) do |item, index|
       item.update_column :position, index
     end
