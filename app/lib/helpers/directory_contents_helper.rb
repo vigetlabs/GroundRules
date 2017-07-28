2c7d823989944298bb220a0b186a0785e6e6f6b4
jroodman <jroodman@users.noreply.github.com>
2017-07-28 16:24:06 -0400

    add test for directory contents helper

--- app/lib/helpers/directory_contents_helper.rb
+++ app/lib/helpers/directory_contents_helper.rb
@@ -2,7 +2,7 @@ module Helpers
 
   class DirectoryContentsHelper
 
-    def self.get_readable_file_names(directory: directory, filetype: filetype)
+    def self.get_readable_file_names(directory:, filetype:)
       Dir.chdir (Rails.root.to_s + directory)
       Dir.glob("*.#{filetype}")
     end
