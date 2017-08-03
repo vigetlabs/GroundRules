a177a9083aae252597d418f543192dddd929e302
Benjamin Modayil <modayilme@gmail.com>
2017-08-03 09:59:02 -0400

    pr fixes part 1

--- app/assets/javascripts/features/machines.js
+++ app/assets/javascripts/features/machines.js
@@ -2,39 +2,44 @@ const buttons = [...document.querySelectorAll('button')]
 const solutions = [...document.querySelectorAll('.problem__solutions')]
 const headers = [...document.querySelectorAll('.device__problems header')]
 
-var first_device = {
-  device: document.querySelector('.device__1'),
-  list: document.querySelector('.device-1__problems')
-}
-var second_device = {
-  device: document.querySelector('.device__2'),
-  list: document.querySelector('.device-2__problems')
-}
-var third_device = {
-  device: document.querySelector('.device__3'),
-  list: document.querySelector('.device-3__problems')
-}
+const devices = [
+  {
+    device: document.querySelector('.device__1'),
+    list: document.querySelector('.device-1__problems')
+  },
+  {
+    device: document.querySelector('.device__2'),
+    list: document.querySelector('.device-2__problems')
+  },
+  {
+    device: document.querySelector('.device__3'),
+    list: document.querySelector('.device-3__problems')
+  }
+]
 
-var devices = [first_device, second_device, third_device]
-
-const removeThis = i => {
-  devices.forEach(i => {
-    i.device.classList.remove('device-selected')
-    i.list.classList.add('__inactive')
+const hideAll = () => {
+  devices.forEach(device => {
+    device.device.classList.remove('device-selected')
+    device.list.classList.add('-inactive')
   })
+}
+
+const addAll = i => {
+  i.device.classList.add('device-selected')
+  i.list.classList.remove('-inactive')
 }
 
 const addThis = i => {
   i.device.classList.add('device-selected')
-  i.list.classList.remove('__inactive')
+  i.list.classList.remove('-inactive')
 }
 
 devices.map(function(i) {
   i.device.onclick = function() {
-    if (this.classList.contains('device-selected')) {
+    if (!this.classList.contains('device-selected')) {
+      hideAll()
+      addAll(i)
     } else {
-      removeThis(i)
-      addThis(i)
     }
   }
 })
@@ -42,8 +47,12 @@ devices.map(function(i) {
 // Problem Statements
 headers.map(x => {
   x.onclick = function() {
-    this.nextSibling.nextSibling.classList.toggle('shown')
-    this.nextSibling.nextSibling.classList.toggle('__inactive-list-item')
-    this.childNodes[2].nextSibling.classList.toggle('-flip-caret')
+    const problem = this.nextSibling.nextSibling.classList
+    const button = this.parentNode.querySelector('button')
+    const caret = this.childNodes[2].nextSibling.classList
+    problem.toggle('shown')
+    problem.toggle('-inactive-list-item')
+    caret.toggle('-flip-caret')
+    button.setAttribute('aria-expanded', 'true')
   }
 })
