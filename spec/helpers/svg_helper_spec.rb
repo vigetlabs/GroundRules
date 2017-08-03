443fe5ff4544c0081c8ecca8484deffffaf737e2
jroodman <jroodman@users.noreply.github.com>
2017-08-03 17:07:22 -0400

    fix tests

--- spec/helpers/svg_helper_spec.rb
+++ spec/helpers/svg_helper_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Helpers::SvgHelper do
+RSpec.describe SvgHelper do
 
-  describe '.build_full_filepath' do
+  describe 'build_full_filepath' do
 
     context 'given a filepath' do
 
       it 'correctly returns a string corresponding to the appropriate file location' do
-        filepath = Helpers::SvgHelper.build_full_filepath('test.html.erb')
+        filepath = helper.build_full_filepath('test.html.erb')
 
         expect(filepath).to eq (Rails.root.to_s + '/app/views/shared/icons/test.html.erb')
       end
