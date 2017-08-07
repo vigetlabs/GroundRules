76787c9e63147465b34d1734fc0b74c1d8aa182a
Benjamin Modayil <modayilme@gmail.com>
2017-08-07 19:57:29 -0400

    fixes from broken merge

--- app/assets/javascripts/features/machines.js
+++ app/assets/javascripts/features/machines.js
@@ -1,78 +1,80 @@
-const main = document.querySelector('main')
-if (main.dataset.jsload) {
-  const buttons = [...document.querySelectorAll('button')]
-  const solutions = [...document.querySelectorAll('.problem__solutions')]
-  const headers = [...document.querySelectorAll('.device__problems header')]
+// const main = document.querySelector('main')
+// if (main.dataset.jsload === true) {
+const buttons = [...document.querySelectorAll('button')]
+const solutions = [...document.querySelectorAll('.problem__solutions')]
+const headers = [...document.querySelectorAll('.device__problems header')]
 
-  const devices = [
-    {
-      device: document.querySelector('.device__1'),
-      list: document.querySelector('.device-1__problems')
-    },
-    {
-      device: document.querySelector('.device__2'),
-      list: document.querySelector('.device-2__problems')
-    },
-    {
-      device: document.querySelector('.device__3'),
-      list: document.querySelector('.device-3__problems')
-    }
-  ]
-
-  const hideAll = () => {
-    devices.forEach(device => {
-      device.device.classList.remove('device-selected')
-      device.list.classList.add('-inactive')
-    })
+const devices = [
+  {
+    device: document.querySelector('.device__1'),
+    list: document.querySelector('.device-1__problems')
+  },
+  {
+    device: document.querySelector('.device__2'),
+    list: document.querySelector('.device-2__problems')
+  },
+  {
+    device: document.querySelector('.device__3'),
+    list: document.querySelector('.device-3__problems')
   }
+]
 
-  const addAll = i => {
-    i.device.classList.add('device-selected')
-    i.list.classList.remove('-inactive')
-  }
-
-  const hideHeaders = () => {
-    headers.forEach(header => {
-      header.nextSibling.nextSibling.classList.remove('shown')
-      header.nextSibling.nextSibling.classList.add('-inactive-list-item')
-      header.childNodes[2].nextSibling.classList.remove('-flip-caret')
-    })
-  }
-
-  devices.map(function(i) {
-    i.device.onclick = function() {
-      if (!this.classList.contains('device-selected')) {
-        hideHeaders()
-        hideAll()
-        addAll(i)
-      } else {
-      }
-    }
-  })
-
-  headers.map(header => {
-    hideHeaders()
-    const problem = header.nextSibling.nextSibling.classList
-    const button = header.parentNode.querySelector('button')
-    const caret = header.childNodes[2].nextSibling.classList
-
-    header.addEventListener('click', () => {
-      if (!problem.contains('shown')) {
-        hideHeaders()
-        problem.add('shown')
-        problem.remove('-inactive-list-item')
-        caret.add('-flip-caret')
-
-        if (button.getAttribute('aria-expanded') == 'false') {
-          button.setAttribute('aria-expanded', 'true')
-        } else {
-          button.setAttribute('aria-expanded', 'false')
-        }
-      } else {
-        problem.remove('shown')
-        problem.add('-inactive-list-item')
-        caret.remove('-flip-caret')
-      }
-    })
+const hideAll = () => {
+  devices.forEach(device => {
+    device.device.classList.remove('device-selected')
+    device.list.classList.add('-inactive')
   })
 }
+
+const addAll = i => {
+  i.device.classList.add('device-selected')
+  i.list.classList.remove('-inactive')
+}
+
+const hideHeaders = () => {
+  headers.forEach(header => {
+    header.nextSibling.nextSibling.classList.remove('shown')
+    header.nextSibling.nextSibling.classList.add('-inactive-list-item')
+    header.childNodes[2].nextSibling.classList.remove('-flip-caret')
+  })
+}
+
+devices.map(i => {
+  console.log(i.device)
+  i.device.addEventListener('click', () => {
+    if (!i.device.classList.contains('device-selected')) {
+      hideHeaders()
+      hideAll()
+      addAll(i)
+    } else {
+    }
+  })
+})
+
+headers.map(header => {
+  hideHeaders()
+  const problem = header.nextSibling.nextSibling.classList
+  const button = header.parentNode.querySelector('button')
+  const caret = header.childNodes[2].nextSibling.classList
+
+  header.addEventListener('click', () => {
+    if (!problem.contains('shown')) {
+      hideHeaders()
+      problem.add('shown')
+      problem.remove('-inactive-list-item')
+      caret.add('-flip-caret')
+
+      if (button.getAttribute('aria-expanded') == 'false') {
+        button.setAttribute('aria-expanded', 'true')
+      } else {
+        button.setAttribute('aria-expanded', 'false')
+      }
+    } else {
+      problem.remove('shown')
+      problem.add('-inactive-list-item')
+      caret.remove('-flip-caret')
+    }
+  })
+})
+// } else {
+// }
--- app/assets/javascripts/features/sliderToggle.js
+++ app/assets/javascripts/features/sliderToggle.js
@@ -1,119 +1,125 @@
 class sliderToggle {
-
   constructor(element) {
-    this.element = element;
-    this.visibleRoast = document.getElementById("roast"+this.element.value);
-    this.visibleBrands = document.getElementById("brand-wrapper"+this.element.value).querySelector('.brands__wrapper');
-    this.colors = ['#F2C287', '#823E2A', '#7A2F22', '#5E180A', '#4A0900', '#330600', '#1A0300', '#0F0300'];
-    this.defaultColor = "#DBDBDB";
-    this.roasts = document.querySelectorAll(".roasts__view");
-    this.brandWrappers = document.querySelectorAll(".roasts__brands-wrapper");
+    this.element = element
+    this.visibleRoast = document.getElementById('roast' + this.element.value)
+    this.visibleBrands = document.getElementById('brand-wrapper' + this.element.value).querySelector('.brands__wrapper')
+    this.colors = ['#F2C287', '#823E2A', '#7A2F22', '#5E180A', '#4A0900', '#330600', '#1A0300', '#0F0300']
+    this.defaultColor = '#DBDBDB'
+    this.roasts = document.querySelectorAll('.roasts__view')
+    this.brandWrappers = document.querySelectorAll('.roasts__brands-wrapper')
 
     for (var i = 0, len = this.roasts.length; i < len; i++) {
-      this.roasts[i].classList.add("hidden");
-      this.brandWrappers[i].classList.add("hidden");
+      this.roasts[i].classList.add('hidden')
+      this.brandWrappers[i].classList.add('hidden')
     }
-    var roast = document.getElementById('roast' + this.element.value);
-    var brand = document.getElementById('brand-wrapper' + this.element.value);
-    if (roast != null) roast.classList.remove("hidden");
-    if (brand != null) brand.classList.remove("hidden");
+    var roast = document.getElementById('roast' + this.element.value)
+    var brand = document.getElementById('brand-wrapper' + this.element.value)
+    if (roast != null) roast.classList.remove('hidden')
+    if (brand != null) brand.classList.remove('hidden')
 
-    this.addEvents();
+    this.addEvents()
   }
 
   addEvents() {
-    this.element.addEventListener('input', this.rangeChange.bind(this));
+    this.element.addEventListener('input', this.rangeChange.bind(this))
   }
 
   rangeChange() {
     for (var i = 0, len = this.roasts.length; i < len; i++) {
-      this.brandWrappers[i].classList.add("hidden");
+      this.brandWrappers[i].classList.add('hidden')
     }
-    var brands = document.getElementById('brand-wrapper' + this.element.value);
-    if (brands != null) brands.classList.remove("hidden");
+    var brands = document.getElementById('brand-wrapper' + this.element.value)
+    if (brands != null) brands.classList.remove('hidden')
 
-    this.updateSlider();
-    this.updateContent(this.element.value, brands);
+    this.updateSlider()
+    this.updateContent(this.element.value, brands)
   }
 
   updateSlider() {
-    var percentage = ((parseFloat(this.element.value))/7)*100 + '%';
-    var backgroundStyles = "linear-gradient(to right, " + this.colors[this.element.value] + " " + percentage + ", " + this.defaultColor + " 0%)";
-    this.element.style.background = backgroundStyles;
+    var percentage = parseFloat(this.element.value) / 7 * 100 + '%'
+    var backgroundStyles =
+      'linear-gradient(to right, ' +
+      this.colors[this.element.value] +
+      ' ' +
+      percentage +
+      ', ' +
+      this.defaultColor +
+      ' 0%)'
+    this.element.style.background = backgroundStyles
   }
 
   updateContent(value, brands) {
-    var brand = brands.querySelector('.brands__wrapper');
+    var brand = brands.querySelector('.brands__wrapper')
 
-    var roastArr = [this.visibleRoast];
-    var brandArr = [this.visibleBrands];
-    var roast = document.getElementById('roast' + value);
-
+    var roastArr = [this.visibleRoast]
+    var brandArr = [this.visibleBrands]
+    var roast = document.getElementById('roast' + value)
 
     if (roastArr != null) {
-      roastArr = this.makeVisible(roast, roastArr, 7, true);
-      this.visibleRoast = roastArr[0];
+      roastArr = this.makeVisible(roast, roastArr, 7, true)
+      this.visibleRoast = roastArr[0]
     }
 
     if (brandArr != null) {
-      brandArr = this.makeVisible(brand, brandArr, 20, false);
-      this.visibleBrands = brandArr[0];
+      brandArr = this.makeVisible(brand, brandArr, 20, false)
+      this.visibleBrands = brandArr[0]
     }
   }
 
   makeVisible(x, vis, timer, bool) {
-    var timerDuration = timer;
-    var op = 1;
-    var y = x;
+    var timerDuration = timer
+    var op = 1
+    var y = x
     if (vis.length > 0) {
       for (var i = 0; i < vis.length; i++) {
-        var z = vis[i];
+        var z = vis[i]
         if (!bool) {
-          y.style.opacity = 0;
-          y.classList.remove("hidden");
+          y.style.opacity = 0
+          y.classList.remove('hidden')
         }
-        if (z == y) return;
+        if (z == y) return
 
-        var timer = setInterval(function () {
+        var timer = setInterval(function() {
           if (op <= 0.1) {
-            clearInterval(timer);
-            z.classList.add("hidden");
-            z.style.opacity = 1;
-            y.style.opacity = 1;
-            y.style.filter = "none";
-            y.classList.remove("hidden");
+            clearInterval(timer)
+            z.classList.add('hidden')
+            z.style.opacity = 1
+            y.style.opacity = 1
+            y.style.filter = 'none'
+            y.classList.remove('hidden')
+          } else {
+            z.style.opacity = op
+            y.style.opacity = 1 - op
+            z.style.filter = 'alpha(opacity=' + op * 100 + ')'
+            y.style.filter = 'alpha(opacity=' + (1 - op) * 100 + ')'
+            op -= op * 0.1
           }
-          else {
-            z.style.opacity = op;
-            y.style.opacity = 1-op;
-            z.style.filter = 'alpha(opacity=' + op * 100 + ")";
-            y.style.filter = 'alpha(opacity=' + (1-op) * 100 + ")";
-            op -= op * 0.1;
-          }
-        }, timerDuration);
+        }, timerDuration)
 
-        op = 1;
-        vis.splice(i, 1);
+        op = 1
+        vis.splice(i, 1)
       }
     } else {
-      y.classList.remove("hidden");
-      y.style.opacity = 1;
-      y.style.filter = "none";
+      y.classList.remove('hidden')
+      y.style.opacity = 1
+      y.style.filter = 'none'
     }
-    vis.push(x);
-    return vis;
-  }
-};
-
-const mainRoasts = document.querySelector('.roasts');
-
-if (mainRoasts.dataset.jsload) {
-  console.log(mainRoasts.dataset);
-  var elements = document.querySelectorAll("[data-feature='sliderToggle']");
-  for (var i=0; i < elements.length; i++) {
-    var element = elements[i];
-    new sliderToggle(element);
+    vis.push(x)
+    return vis
   }
 }
 
-module.exports = sliderToggle;
+// const mainRoasts = document.querySelector('.roasts')
+
+// if (mainRoasts.dataset.jsload) {
+// console.log(mainRoasts.dataset)
+var elements = document.querySelectorAll("[data-feature='sliderToggle']")
+for (var i = 0; i < elements.length; i++) {
+  var element = elements[i]
+  new sliderToggle(element)
+}
+// } else {
+//   alert('where?')
+// }
+
+module.exports = sliderToggle
