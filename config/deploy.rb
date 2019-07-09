e393555087d51ebab9388d4583c8df3579baec03
Nathan Hunzaker <nate.hunzaker@gmail.com>
2019-07-09 09:13:18 -0700

    Remove slack deploy configuration
    Slack invalidated this hook because it is exposed publically. While we figure out the best path forward, I've removed the slack configuration.

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,13 +4,6 @@ set :user, "deploy"
 set :default_stage, 'production'
 set :branch, 'master'
 
-# Enable slack notifications about deploy
-set :slack_url,     'https://hooks.slack.com/services/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'
-set :slack_channel, '#intern17-only'
-set :slack_username, 'GroundRules Deploy'
-set :slack_emoji, ':coffee:'
-set :slack_app_url, 'http://www.groundrules.co/'
-
 # Install Node dependencies before precompiling assets
 namespace :deploy do
   namespace :npm do
